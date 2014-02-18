ff31a3585e34d96dc75deb16d0eca570fa1a463b
Raven Jiang <raven@cs.stanford.edu>
2014-02-17 20:06:00 -0800

    Added Devise.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,10 +60,12 @@ Hirefaq::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.initialize_on_precompile = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'hirefaq.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
